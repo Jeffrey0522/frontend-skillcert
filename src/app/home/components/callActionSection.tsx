e1643cac33163d7e7f5f74e57b8c34800bdaff6c
Ezeh20 <ranter1570@gmail.com>
2025-02-21 03:14:37 +0100

    chore: extracted static texts into an object

--- src/app/home/components/callActionSection.tsx
+++ src/app/home/components/callActionSection.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+const staticText = {
+	heading: "Ready to Start Learning?",
+	subText: "Join thousands of students and instructors in our growing community.",
+};
+
 const CallActionSection = () => {
 	return (
-		<section>
-			<p> CallActionSection</p>
+		<section className="flex flex-col items-center gap-5 justify-center p-5 bg-gray-900">
+			<h2 className=" text-purple-700 text-3xl font-medium">{staticText.heading}</h2>
+			<p className=" text-gray-400 text-xl">{staticText.subText}</p>
 		</section>
 	);
 };
