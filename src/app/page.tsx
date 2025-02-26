314dbd87706909d27fa7d809ea6fb90e9e870d31
CristopherAguilar10 <jankaguilar1030@gmail.com>
2025-02-25 23:02:10 -0600

    feat: add button to principal page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "../components/Button";
+import ConnectWallet from "@/app/components/ConnectWalletButton";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="mx-auto container flex justify-center mt-20">
-      <Button>Button</Button>
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+      <ConnectWallet />
     </div>
   );
 }
