a8193d0c3b6d2b4ba28376accd8809f6deb264d2
bitfalt <75431447+bitfalt@users.noreply.github.com>
2025-02-27 12:46:07 -0600

    add: wallet connection

---
+++ src/wallet/walletHooks.ts
@@ -0,0 +1,32 @@
+import { kit } from './walletKit';
+import { ISupportedWallet } from '@creit.tech/stellar-wallets-kit';
+import { useWalletStore } from '@/store/walletStore'; 
+
+export const useWallet = () => {
+  const { connectWalletStore, disconnectWalletStore } = useWalletStore();
+
+  const connectWallet = async () => {
+    await kit.openModal({
+      modalTitle: 'Connect to your favorite wallet',
+      onWalletSelected: async (option: ISupportedWallet) => {
+        kit.setWallet(option.id);
+
+        const { address } = await kit.getAddress();
+        const { name } = option;
+
+        connectWalletStore(address, name);
+      },
+    });
+  };
+
+  const disconnectWallet = async () => {
+    await kit.disconnect();
+    disconnectWalletStore();
+  };
+
+  return {
+    connectWallet,
+    disconnectWallet,
+  };
+};
+
