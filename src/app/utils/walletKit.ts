dd70ffc8a0b533feeb38512720a1ac2dad119a11
CristopherAguilar10 <jankaguilar1030@gmail.com>
2025-02-26 18:22:16 -0600

    feat: change comments

--- src/app/utils/walletKit.ts
+++ src/app/utils/walletKit.ts
@@ -6,8 +6,8 @@ import {
   } from "@creit.tech/stellar-wallets-kit";
   
   export const walletKit = new StellarWalletsKit({
-    network: WalletNetwork.TESTNET, // O WalletNetwork.PUBLIC para producción
-    selectedWalletId: XBULL_ID, // Valor por defecto; se cargan todos los módulos
-    modules: allowAllModules(), // Carga todos los módulos: Albedo, Freighter, Hana, Ledger, Trezor, Lobstr, Rabet, WalletConnect, xBull, HOT Wallet
+    network: WalletNetwork.TESTNET, 
+    selectedWalletId: XBULL_ID, 
+    modules: allowAllModules(), 
   });
   
