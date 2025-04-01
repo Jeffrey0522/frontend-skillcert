7613e47614786cc459ba4cf6b8757c1b26144b1e
Jagadeeshftw <jagadeesh26062002@gmail.com>
2025-04-01 17:34:08 +0530

    feat: add db and env configs

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,20 +2,62 @@
 // It initializes the application, sets up the database connection, and starts the server.
 
 use actix_web::{web, App, HttpServer};
-use sqlx::PgPool;
+use tokio::net::TcpListener;
 
 mod models;
+
+mod config;
+mod db;
+use crate::config::Configuration;
+use crate::db::Db;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = PgPool::connect(&database_url).await.expect("Failed to connect to the database");
 
-    HttpServer::new(move || {
+    // Read (development) Environment Variables.
+    dotenvy::dotenv().ok();
+
+    // App configuration.
+    tracing::debug!("Initializing configuration");
+    let config = Configuration::new();
+
+    println!("Configuration: {:?}", config);
+    // Initialize DB connection.
+    tracing::debug!("Initializing DB pool");
+    let db = Db::new(&config.db_str, config.db_pool_max_size)
+    .await
+    .unwrap_or_else(|err| panic!("Failed to initialize DB: {:?}", err));
+
+    // Run migrations.
+    tracing::debug!("Running Migrations");
+    db.migrate().await.expect("Failed to run migrations");
+
+    // Listen for requests on specified port.
+    tracing::info!("Starting server on {}", config.listen_address);
+    let listener = TcpListener::bind(&config.listen_address)
+        .await
+        .expect("Failed to bind address");
+
+    // // Start the server with the /users route.
+    // tracing::info!("Starting server on {}", config.listen_address);
+    // HttpServer::new(move || {
+    //     App::new()
+    //         // In case your get_users expects a PgPool, ensure that you pass it.
+    //         // For example, if your Db type exposes the pool as `db.pool` and implements Clone:
+    //         .app_data(web::Data::new(db.clone()))
+    //         .route("/users", web::get().to(models::user::get_users))
+    // })
+    // .bind(config.listen_address)?
+    // .run()
+    // .await
+
+
+    HttpServer::new(|| {
         App::new()
-            .data(pool.clone())
-            .route("/users", web::get().to(models::user::get_users))
+            .service(web::resource("/").to(|| async { "hello world" }))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("127.0.0.1", 8081))?
     .run()
     .await
+
 }
